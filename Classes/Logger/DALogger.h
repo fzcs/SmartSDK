94496484d6c6d36a6f8b908dd629e31b249f7f5c
fzcs <fzcs@live.cn>
2014-05-12 16:08:31 +0800

    添加 AFNetworking log 模块 依赖

--- Classes/Logger/DALogger.h
+++ Classes/Logger/DALogger.h
@@ -5,6 +5,17 @@
 //  Created by 罗浩 on 14-5-12.
 //  Copyright (c) 2014年 DreamArts. All rights reserved.
 //
+
+
+//==========将下列代码添加到到 APPNAME-Prefix.pch 中==========
+//    #import "DALogger.h"
+//    #ifdef DEBUG
+//        static const int ddLogLevel = LOG_LEVEL_VERBOSE;
+//    #else
+//        static const int ddLogLevel = LOG_LEVEL_WARN;
+//    #endif
+//=========================End============================
+
 #import <Foundation/Foundation.h>
 #import "DALoggerHeader.h"
 
